89f61d8622ce7e4180ef97721205261cf1c42e18
bantawa04 <rahunn3@gmail.com>
2023-02-18 15:59:05 +0545

    feat: setup cors headers

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	"example.com/blog/models"
+	"example.com/blog/setup"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -13,11 +13,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	if err := models.ConnectToDB(); err != nil {
+	if err := setup.ConnectToDB(); err != nil {
 		panic(err)
 	}
 	//Go router declaration
 	r := gin.Default()
+	r.Use(setup.CorsMiddleware())
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"ping": "pong"})
--- setup/db.go
+++ setup/db.go
@@ -1,4 +1,4 @@
-package models
+package setup
 
 import (
 	"database/sql"
